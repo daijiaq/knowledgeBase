b41714a3ff3978740ef159cca85118eeb7dbeade
wwwwxus <2976370121@qq.com>
2025-06-22 18:56:44 +0800

    style:编辑器引入字体图标

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -5,83 +5,122 @@
                 <button @click="editor.chain().focus().toggleBold().run()"
                     :disabled="!editor.can().chain().focus().toggleBold().run()"
                     :class="{ 'is-active': editor.isActive('bold') }">
-                    加粗
+                    <span class="iconfont icon-bold"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleItalic().run()"
                     :disabled="!editor.can().chain().focus().toggleItalic().run()"
                     :class="{ 'is-active': editor.isActive('italic') }">
-                    斜体
+                    <span class="iconfont icon-italic"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleStrike().run()"
                     :disabled="!editor.can().chain().focus().toggleStrike().run()"
                     :class="{ 'is-active': editor.isActive('strike') }">
-                    删除
+                    <span class="iconfont icon-strikethrough"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleCode().run()"
                     :disabled="!editor.can().chain().focus().toggleCode().run()"
                     :class="{ 'is-active': editor.isActive('code') }">
-                    代码
+                    <span class="iconfont icon-code-view"></span>
                 </button>
-                <button @click="editor.chain().focus().unsetAllMarks().run()">Clear marks</button>
-                <button @click="editor.chain().focus().clearNodes().run()">Clear nodes</button>
                 <button @click="editor.chain().focus().setParagraph().run()"
                     :class="{ 'is-active': editor.isActive('paragraph') }">
-                    段落
+                    <span class="iconfont icon-duanla"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
                     :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
-                    H1
+                    <span class="iconfont icon-13biaoti1"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
                     :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
-                    H2
+                    <span class="iconfont icon-14biaoti2"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
                     :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
-                    H3
+                    <span class="iconfont icon-15biaoti3"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
                     :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
-                    H4
+                    <span class="iconfont icon-16biaoti4"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
                     :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }">
-                    H5
+                    <span class="iconfont icon-17biaoti5"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
                     :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }">
-                    H6
+                    <span class="iconfont icon-18biaoti6"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleBulletList().run()"
                     :class="{ 'is-active': editor.isActive('bulletList') }">
-                    无序列表
+                    <span class="iconfont icon-list-unordered"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleOrderedList().run()"
                     :class="{ 'is-active': editor.isActive('orderedList') }">
-                    有序列表
+                    <span class="iconfont icon-list-ordered"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleCodeBlock().run()"
                     :class="{ 'is-active': editor.isActive('codeBlock') }">
-                    代码块
+                    <span class="iconfont icon-daimakuaicodeblock"></span>
                 </button>
                 <button @click="editor.chain().focus().toggleBlockquote().run()"
                     :class="{ 'is-active': editor.isActive('blockquote') }">
-                    块引用
+                    <span class="iconfont icon-double-quotes-l"></span>
                 </button>
-                <button @click="editor.chain().focus().setHorizontalRule().run()">分割线</button>
-                <button @click="editor.chain().focus().setHardBreak().run()">换行</button>
+                <button @click="editor.chain().focus().setHorizontalRule().run()">
+                    <span class="iconfont icon-split-cells-vertical"></span>
+                </button>
+                <button @click="editor.chain().focus().setHardBreak().run()">
+                    <span class="iconfont icon-text-wrap"></span>
+                </button>
                 <button @click="editor.chain().focus().undo().run()"
                     :disabled="!editor.can().chain().focus().undo().run()">
-                    上一步
+                    <span class="iconfont icon-25chehui"></span>
                 </button>
                 <button @click="editor.chain().focus().redo().run()"
                     :disabled="!editor.can().chain().focus().redo().run()">
-                    下一步
+                    <span class="iconfont icon-26quxiaochehui"></span>
                 </button>
                 <button @click="editor.chain().focus().setColor('#958DF1').run()"
                     :class="{ 'is-active': editor.isActive('textStyle', { color: '#958DF1' }) }">
-                    紫色
+                    <span class="iconfont icon-24zitiyanse"></span>
                 </button>
+                <button @click="editor.chain().focus().setTextAlign('left').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }">
+                    <span class="iconfont icon-align-left"></span>
+                </button>
+                <button @click="editor.chain().focus().setTextAlign('center').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }">
+                    <span class="iconfont icon-align-center"></span>
+                </button>
+                <button @click="editor.chain().focus().setTextAlign('right').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }">
+                    <span class="iconfont icon-align-right"></span>
+                </button>
+                <button @click="editor.chain().focus().setTextAlign('justify').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'justify' }) }">
+                    <span class="iconfont icon-align-justify"></span>
+                </button>
+                <button @click="setLink" :class="{ 'is-active': editor.isActive('link') }">
+                    <span class="iconfont icon-link"></span>
+                </button>
+                <button @click="editor.chain().focus().unsetLink().run()" :disabled="!editor.isActive('link')">
+                    <span class="iconfont icon-link-unlink"></span>
+                </button>
+                <button @click="editor.chain().focus().toggleUnderline().run()" :class="{ 'is-active': editor.isActive('underline') }">
+                    <span class="iconfont icon-font-color"></span>
+                </button>
+                <div class="color-picker">
+                <button>
+                    <span class="iconfont icon-19beijingyanse"></span>
+                </button>
+                <!-- 预设颜色按钮 -->
+                <button 
+                    v-for="color in highlightColors" 
+                    :key="color" 
+                    :style="{ backgroundColor: color }" 
+                    @click="editor.chain().focus().setHighlight({ color }).run()"
+                ></button>
+                <!-- 清除背景色按钮 -->
+                <button @click="editor.chain().focus().unsetHighlight().run()">
+                    <span class="iconfont icon-jinzhi"></span>
+                </button>
+                </div>
             </div>
         </div>
         <button @click="saveText">保存</button>
@@ -96,23 +135,75 @@ import { ListItem } from '@tiptap/extension-list-item'
 import { TextStyle } from '@tiptap/extension-text-style'
 import { Color } from '@tiptap/extension-color'
 import StarterKit from '@tiptap/starter-kit'
+import Highlight from '@tiptap/extension-highlight'
+import TextAlign from '@tiptap/extension-text-align'
+import Link from '@tiptap/extension-link'
+import Underline from '@tiptap/extension-underline'
 import { useEditor, EditorContent , Editor } from '@tiptap/vue-3'
-import { onBeforeUnmount } from 'vue'
+import { onBeforeUnmount , ref } from 'vue'
 
+// 预设颜色列表（可自定义）
+const highlightColors = ref(['#E0F7FA', '#FFF3E0', '#FFF8E1', '#F0F4C3', '#FFE0B2','#EDE7F6'])
+// 当前对齐状态（初始左对齐）
+const currentAlignment = ref('left') 
 
 const editor = useEditor({
     extensions: [
         Color.configure({ types: [TextStyle.name, ListItem.name] }),
         TextStyle.configure({ types: [ListItem.name] }),
+        Highlight.configure({
+            multicolor: true, // 启用多颜色
+        }),
+        TextAlign.configure({
+          types: ['heading', 'paragraph'],
+        }),
+        Link.configure({
+          openOnClick: false,
+          defaultProtocol: 'https',
+        }),
         StarterKit,
+        Underline,
     ],
-    content: `<h1>hello<h1/>`,
+    content: `<h2>Heading</h2>
+        <p style="text-align: center">first paragraph</p>
+        <p style="text-align: right">second paragraph</p>`,
     onUpdate({ editor }) {
         //自动获取数据内容
         const json = editor.getJSON()
         console.log(json)
     },
 })
+
+function setLink() {
+    const previousUrl = editor.value.getAttributes('link').href
+    const url = window.prompt('URL', previousUrl)
+
+    // cancelled
+    if (url === null) {
+    return
+    }
+
+    // empty
+    if (url === '') {
+    editor.value
+        .chain()
+        .focus()
+        .extendMarkRange('link')
+        .unsetLink()
+        .run()
+
+    return
+    }
+
+    // update link
+    editor.value
+    .chain()
+    .focus()
+    .extendMarkRange('link')
+    .setLink({ href: url })
+    .run()
+}
+
 function saveText(){
     //手动获取文本内容
     console.log(editor.value.getJSON());
@@ -170,6 +261,10 @@ onBeforeUnmount(() => {
 </script>
 
 <style lang="scss">
+.container{
+    width: 80%;
+    margin:0 auto;
+}
 .button-group {
     button {
     /* 默认样式（未激活时） */
@@ -193,6 +288,18 @@ onBeforeUnmount(() => {
       background: #eee;
     }
   }
+    .color-picker {
+        display: flex;
+        gap: 8px;
+        margin-bottom: 16px;
+        button {
+            min-width: 32px;
+            height: 32px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+    }
 }
 /* Basic editor styles */
 .tiptap {
@@ -227,6 +334,14 @@ onBeforeUnmount(() => {
         margin: 1.5rem 0;
         padding-left: 1rem;
     }
+    a {
+    color: #958DF1;
+    text-decoration: underline;
+    cursor: pointer;
+    &:hover {
+      color: #8A82E8;
+    }
+  }
     
 }
 .editor-content {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
+import './public/style.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import router from './router'
 import 'element-plus/dist/index.css'
+import './public/iconfont.css'
 
 const app=createApp(App)
 app.use(createPinia())
