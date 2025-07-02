0335c5f37258a11b9fcd926744e508bdce4cfadc
wwwwxus <2976370121@qq.com>
2025-07-02 22:25:10 +0800

    style:修复前期修改功能bug导致的样式丢失问题

--- src/pages/editor.vue
+++ src/pages/editor.vue
@@ -1,11 +1,19 @@
 <template>
-  <CollaborativeEditor
+  <div class="editor-page">
+    <CollaborativeEditor
     :key="String($route.params.documentId)"
     :docId="Number($route.params.documentId)"
     :roomId="String($route.params.documentId)"
   />
-  <Comment :key="String($route.params.documentId)" />
+    <Comment :key="String($route.params.documentId)" />
+  </div>
 </template>
 
-<script lang="ts" setup></script>
-<style lang=""></style>
+<script lang="ts" setup>
+</script>
+<style lang="css" scoped>
+.editor-page {
+  display: flex;
+  height: 100%;
+}
+</style>
