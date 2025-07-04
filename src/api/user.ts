66712eb190dd8101cff288243c0a930579d2b894
nanshengcarol <157878323+nanshengcarol@users.noreply.github.com>
2025-07-04 20:07:30 +0800

    feat:光标改成用户名

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,35 +1,40 @@
-import request from '../utils/request'
+import request from "../utils/request";
+import type { userResponse } from "../types/user";
 // 登录
 export const userLogin = (email: string, password: string) => {
-    return request({
-        url: '/users/login',
-        method: 'POST',
-        data: { email, password }
-    })
-}
+  return request({
+    url: "/users/login",
+    method: "POST",
+    data: { email, password },
+  });
+};
 // 注册
-export const userRegister = (username: string, email: string, password: string) => {
-    return request({
-        url: '/users/register',
-        method: 'POST',
-        data: { username, email, password }
-    })
-}
+export const userRegister = (
+  username: string,
+  email: string,
+  password: string
+) => {
+  return request({
+    url: "/users/register",
+    method: "POST",
+    data: { username, email, password },
+  });
+};
 //根据邮箱搜索用户
-export const userSearch = (keyword:string,knowledgeBaseId:number)=>{
-    return request({
-        url:'/users/search',
-        method:'GET',
-        params:{
-            email:keyword,
-            knowledgeBaseId
-        }
-    })
-}
+export const userSearch = (keyword: string, knowledgeBaseId: number) => {
+  return request({
+    url: "/users/search",
+    method: "GET",
+    params: {
+      email: keyword,
+      knowledgeBaseId,
+    },
+  });
+};
 
-export const getUserInfo = (): Promise<any> => {
-    return request({
-        url:'/users/profile',
-        method:'GET',
-    })
-}
+export const getUserInfo = (): Promise<userResponse> => {
+  return request({
+    url: "/users/profile",
+    method: "GET",
+  });
+};
--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -5,24 +5,39 @@ interface userRegister {
   /**
    * 用户名称
    */
-  username: string
+  username: string;
   /**
    * 邮箱
    */
-  email: string
+  email: string;
   /**
    * 密码
    */
-  password: string
+  password: string;
 }
 
 // 用户信息
-export interface userInfo{
-  username:string
-  id:number
-  email:string
+export interface userInfo {
+  username: string;
+  id: number;
+  email: string;
 }
 //搜索的列表信息
-export interface searchItem extends userInfo{
-  checked:boolean
+export interface searchItem extends userInfo {
+  checked: boolean;
+}
+
+export interface userResponse {
+  code: number;
+  data: userResponseData;
+  message: string;
+}
+
+export interface userResponseData {
+  createdAt: string;
+  email: string;
+  id: number;
+  isActive: boolean;
+  updatedAt: string;
+  username: string;
 }
